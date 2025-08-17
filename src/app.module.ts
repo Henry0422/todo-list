14438c36143d7ca0dd0e0a913c59d6ff49ae55f5
Henry Han <henry.han@usask.ca>
2025-08-16 22:55:13 -0400

    Generate lists resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ListsModule } from './lists/lists.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, ListsModule],
   controllers: [AppController],
   providers: [AppService],
 })
