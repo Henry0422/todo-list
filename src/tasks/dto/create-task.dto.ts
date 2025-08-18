1ba03f8acbb02f0857161c8bdd657b322e49278b
Henry Han <henry.han@usask.ca>
2025-08-17 22:20:00 -0400

    Add input validation for tasks

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,11 +1,29 @@
+import { Type } from "class-transformer";
+import { IsDateString, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, MaxLength, MinDate, MinLength } from "class-validator";
 import { Status } from "generated/prisma";
 
 export class CreateTaskDto {
+    @IsString()
+    @MinLength(2)
     title: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(300)
     description?: string;
-    status: Status
+
+    @IsEnum(Status)
+    status: Status;
+
+    @IsDateString()
+    @Type(() => Date)
+    @MinDate(new Date())
     dueDate: Date;
+
     createdAt: Date;
     updatedAt: Date;
+
+    @IsInt()
+    @IsNotEmpty()
     listId: number;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, ParseIntPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -18,20 +18,20 @@ export class TasksController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const task = await this.tasksService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const task = await this.tasksService.findOne(id);
     if (!task)
       throw new NotFoundException(`Article with id: ${id} not found`);
     return task;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id , updateTaskDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.tasksService.update(id , updateTaskDto);
   } 
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.remove(id);
   }
 }
