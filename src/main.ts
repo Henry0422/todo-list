47671072a431eeccf3dab10703565bb72a81f18f
Henry Han <henry.han@usask.ca>
2025-08-21 23:05:03 -0400

    Add CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Accept'],
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
 
   app.useGlobalPipes(new ValidationPipe());
 }
